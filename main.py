49ad72454c723ffe32066317c760fad4cf625d0a
KuVe076 <gonzalo.alarconc@usm.cl>
2024-09-08 03:53:07 -0300

    #8 Terminado CRUD y actualizacion estado

--- main.py
+++ main.py
@@ -29,7 +29,7 @@ def main():
     MakeTareasBD()
     if authentication:
         while True:
-            menu = int(input("Selecciona una opción (1 para crear una tarea, 2 para consultar una tarea, 3 para actualizar una tarea, 4 para eliminar una tarea, 5 para salir): "))
+            menu = int(input("Selecciona una opción (1 para crear una tarea, 2 para ver las tareas, 3 para actualizar una tarea, 4 para eliminar una tarea, 5 para cambiar estado de una tarea, 6 para salir): "))
 
             if menu == 1:
                 title = input("Titulo: ")
@@ -38,24 +38,52 @@ def main():
                 tag = input("Etiquieta: ")
                 try:
                     fecha = datetime.datetime.strptime(date, "%Y-%m-%d").date()
-                    print(fecha)
                 except ValueError:
                     print("Formato de fecha incorrecto. Use el formato YYYY-MM-DD.")
                 MakeIssue(title,description,fecha,tag)
 
             elif menu == 2:
-                print("ACA SE DEBERIA PODER CONSULTAR")
+                ViewIssue()
 
             elif menu == 3:
-                print("ACA SE DEBERIA ACTUALIZAR TAREA") # HAERLO POR ID
+                id_issue = int(input("Ingresa el ID del Issue a modificar: "))
+                while True:
+                    select = int(input("Selecciona una opcion(1 Titulo, 2 Descripción, 3 Fecha Vencimiento, 4 Tag, 5 Salir): "))
+                    if select == 1:
+                        title = input("Ingresa el nuevo titulo: ")
+                        UpdateIssueTitle(id_issue,title)
+                    elif select == 2:
+                        desc = input("Ingresa nueva descripción: ")
+                        UpdateIssueDesc(id_issue,desc)
+                    elif select == 3:
+                        fecha = input("Ingresa nueva fecha de vencimiento(YYYY-MM-DD): ")
+                        try:
+                            format_fecha = datetime.datetime.strptime(fecha, "%Y-%m-%d").date()
+                        except ValueError:
+                            print("Formato de fecha incorrecto. Use el formato YYYY-MM-DD.")
+                        UpdateIssueDate(id_issue,format_fecha)
+                    elif select == 4:
+                        tag = input("Ingresa el nuevo Tag: ")
+                        UpdateIssueTag(id_issue,tag)
+                    elif select == 5:
+                        break
+                    else:
+                        print("NUMERO INCORRECTO")
 
             elif menu == 4:
+                id_issue = int(input("Ingresa el ID de la tarea que deseas eliminar: "))
+                DeleteIssue(id_issue)
 
-                print("ACA SE DEBERIA ELIMINAR TAREA") #HACER POR ID
             elif menu == 5:
+                id_issue = int(input("Ingresa el ID del Issue a modificar: "))
+                new_status = input("Ingresa el estado nuevo de la tarea: ")
+                UpdateIssueStatus(id_issue,new_status)
+
+
+            elif menu == 6:
                 print("ADIOS")
                 break
-            
+
             else:
                 print("NUMERO INCORRECTO")
 
--- tareas.py
+++ tareas.py
@@ -21,3 +21,38 @@ def MakeIssue(titulo,desc,date,tag):
     issue_info = [titulo,desc,fecha,tag,"pendiente"]
     cursor.execute("INSERT INTO issues (titulo,descripcion,vencimiento,etiqueta,estado) VALUES (?,?,?,?,?)", issue_info)
     print("TAREA AGREGADA CORRECTAMENTE")
+
+def ViewIssue():
+    cursor.execute("SELECT * FROM issues")
+    result = cursor.fetchall()
+    if len(result) != 0:
+        print("Tareas:")
+        for i in result:
+            print(f"id_tarea: {i[0]}: Titulo: {i[1]}, Descripción: {i[2]}, Fecha de Vencimiento: {i[3]}, Etiquieta: {i[4]}, Estado: {i[5]}.")  
+    else:
+        print("No tienes tareas")
+
+def UpdateIssueTitle(id_issue,name):
+    cursor.execute("UPDATE issues SET titulo=? WHERE id_tarea =?", name, id_issue)
+
+def UpdateIssueDesc(id_issue,desc):
+    cursor.execute("UPDATE issues SET descripcion=? WHERE id_tarea =?", desc, id_issue)
+
+def UpdateIssueDate(id_issue,date):
+    fecha = date.strftime("%Y-%m-%d")
+    cursor.execute("UPDATE issues SET vencimiento=? WHERE id_tarea =?", fecha, id_issue)
+
+def UpdateIssueTag(id_issue,tag):
+    cursor.execute("UPDATE issues SET etiqueta=? WHERE id_tarea =?", tag, id_issue)
+
+def DeleteIssue(id_issue):
+    cursor.execute("SELECT * FROM issues WHERE id_tarea=?",id_issue)
+    result = cursor.fetchone()
+    if result is not None:
+        cursor.execute("DELETE FROM issues WHERE id_tarea=?", id_issue)
+        print("LA TAREA FUE ELIMINADA CON EXITO")
+    else:
+        print("La tarea con ese ID no existe")
+
+def UpdateIssueStatus(id_issue,new_status):
+    cursor.execute("UPDATE issues SET estado=? WHERE id_tarea =?", new_status, id_issue)
